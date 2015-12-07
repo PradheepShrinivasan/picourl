ee7a4d844c838ac95b100dd7486f582402414ecb
Pradheep Shrinivasan <pradheep.sh@gmail.com>
2015-12-06 22:29:14 -0600

    added the model of the url shortener

---
+++ app/models/test_urlShortener.py
@@ -0,0 +1,70 @@
+import unittest
+import pymongo
+
+from urlshortener import urlShortener
+
+
+class TestUrlShortener(unittest.TestCase):
+    def setUp(self):
+        connection = pymongo.MongoClient('mongodb://localhost:27017/')
+        self.database = connection.test
+        self.collection = self.database.urlshortener
+        self.urlShortener = urlShortener(self.collection)
+
+    def test_saveUrl_Unique(self):
+        # setup
+        url = "http://www.google.com"
+        shortUrl = "gl"
+
+        result = self.urlShortener.saveUrl(shortUrl, url)
+
+        # Assertions
+        self.assertEqual(result, True)
+        doc = self.collection.find_one({'_id': shortUrl})
+        self.assertEqual(doc, {'_id': shortUrl, 'url': url})
+
+        # cleanup so that next time we dont get duplicateKeyError
+        self.collection.delete_one({'_id': shortUrl})
+
+    def test_saveUrl_duplicate(self):
+        shortUrl = 'orig'
+        url = 'http://www.google.com'
+        urldup = 'http://www.yahoo.com'
+
+        self.urlShortener.saveUrl(shortUrl, url)
+        result = self.urlShortener.saveUrl(shortUrl, urldup)
+
+        self.assertEqual(result, False)
+        doc = self.collection.find_one({'_id': shortUrl})
+        self.assertEqual(doc, {'_id': shortUrl, 'url': url})
+
+        self.collection.delete_one({'_id': shortUrl})
+
+    def test_findUrl_Existing(self):
+
+        shortUrl = 'findUrl'
+        url = 'http://www.google.com'
+        self.urlShortener.saveUrl(shortUrl, url)
+
+        resultUrl = self.urlShortener.findUrl(shortUrl)
+
+        self.assertEqual(resultUrl, url)
+
+        self.collection.delete_one({'_id': shortUrl})
+
+    def test_findUrl_NonExisting(self):
+        shortUrl = 'findDuplicate'
+
+        resultUrl = self.urlShortener.findUrl(shortUrl)
+
+        self.assertEqual(resultUrl, None)
+
+    def test_generateRandom(self):
+
+        self.assertEqual(len(self.urlShortener.generateShortUrl()), 6)
+        self.assertEqual(len(self.urlShortener.generateShortUrl(7)), 7)
+
+        self.assertEqual(self.urlShortener.generateShortUrl().isalnum(), True)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ app/models/urlshortener.py
@@ -0,0 +1,48 @@
+import random
+import string
+
+class urlShortener:
+
+    def __init__(self, collection):
+        self.collection = collection
+
+    #  Save short Url and url
+    # The short Url is stored as index as all looks
+    # find and deletes will be only using short Url
+    def saveUrl(self, shortUrl, url):
+        saveQuery = {'_id': shortUrl, 'url':url}
+
+        try:
+            self.collection.insert_one(saveQuery)
+        except:
+                return False
+        return True
+
+
+    # Finds a url from shorUrl that is sent from the user
+    def findUrl(self, shortUrl):
+
+        try:
+            doc  = self.collection.find_one({'_id': shortUrl})
+        except:
+            return None
+        if doc is None:
+            return None
+
+        return doc['url']
+
+    def removeUrl(self, shortUrl):
+
+        try:
+            result = self.collection.delete_one({'_id': shortUrl})
+        except:
+            # we would want to log an error message as of now
+            pass
+
+
+    # generates an shorturl needed from so
+    #  http://stackoverflow.com/questions/2257441/random-string-generation-with-upper-case-letters-and-digits-in-python/23728630#23728630
+    def generateShortUrl(self, length=6):
+        return ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(length))
+
+

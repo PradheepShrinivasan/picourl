d3b3a2f648ba73eb2077677f36f8a028961214e9
Pradheep Shrinivasan <pradheep.sh@gmail.com>
2015-12-22 23:41:02 -0600

    removed unused os

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 #!flask/bin/python
-import os
 
 from app import app
 from config import PORT
 
-app.run(host='0.0.0.0', port=int(PORT),debug=True)
+app.run(host='0.0.0.0', port=int(PORT), debug=True)
